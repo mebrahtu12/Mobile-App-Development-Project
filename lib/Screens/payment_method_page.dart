456ba0ba2a780f6272e6f76fa9561129b93998df
Junior <juniorbek21@gmail.com>
2026-04-18 21:35:47 +0300

    added another payment method

---
+++ lib/Screens/payment_method_page.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class PaymentMenthodPage extends StatelessWidget {
+  const PaymentMenthodPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment Method'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          SizedBox(
+            height: 39,
+          ),
+          Text(
+            'Wellcome to Payment Method',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 39,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Pay Now'))
+        ],
+      )),
+    );
+  }
+}
